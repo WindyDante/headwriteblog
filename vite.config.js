c474bfbb7c7ad3bbce7423b078deb2f1583fc637
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-09-25 15:52:14 +0800

    test blog online

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/headwriteblog/",
   plugins: [
     vue(),
   ],
+  build:{
+    outDir:"docs"
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
