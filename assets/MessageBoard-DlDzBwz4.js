23bd9a2265a818d86df78d355754c367e90f7f7d
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-10-11 07:07:38 +0000

    Deploying to gh-pages from @ WindyDante/headwriteblog@ca6dffec44913d8ed584e182c87a2709e6722827 🚀

--- assets/MessageBoard-DlDzBwz4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,r,c as s,o as t}from"./index-BTkXt9Ic.js";const o={setup(){return r(),{}}};function a(c,n,p,_,f,u){return t(),s("div",null," 留言板 ")}const l=e(o,[["render",a]]);export{l as default};
