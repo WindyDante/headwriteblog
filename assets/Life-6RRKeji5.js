2b17328fefc5729c429444da8039af604fad17f4
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-10-11 12:26:08 +0800

    发布于：2024-10-11 12:26:07

--- assets/Life-6RRKeji5.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as o,c as r,a as s,o as a}from"./index-CD-1A5P9.js";const n={setup(){return o(),{}}},c={class:"container"};function p(_,e,d,l,f,i){return a(),r("div",c,e[0]||(e[0]=[s("h1",null,"勇敢面对生活",-1),s("p",null," 无论生活如何挑战我们，我们都需要勇敢面对。只有这样才能克服困难，实现自己的目标和价值。 ",-1)]))}const m=t(n,[["render",p],["__scopeId","data-v-88805e63"]]);export{m as default};
