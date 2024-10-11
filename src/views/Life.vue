b399e5529f43be9a2142230560d1fd2ab9ed9d45
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-10-11 10:26:32 +0800

    init auto deploy

--- src/views/Life.vue
+++ src/views/Life.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <!-- -->
-    <h1>this is my life......</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-body">
+              <h1 class="card-title">Life</h1>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <p class="card-text">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
   </div>
 </template>
 
