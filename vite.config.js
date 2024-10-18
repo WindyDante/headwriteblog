d4819353ca6e63afd9224adc3f8fa6d3527b97f9
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-10-18 09:30:56 +0800

    fix md not plugins for vite issues

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import md from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"/headwriteblog/",
   plugins: [
     vue(),
+    md()
   ],
+  assetsInclude: ['**/*.md'],
   build:{
     outDir:"docs"
   },
